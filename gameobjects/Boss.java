89b01651e63a928f6e69edf3ac7ef867de038d99
Max <korobeynikovmax@gmail.com>
2021-03-11 16:57:25 +0300

    boss animation

--- gameobjects/Boss.java
+++ gameobjects/Boss.java
@@ -1,5 +1,6 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.games.spaceinvaders.Direction;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
 public class Boss extends EnemyShip {
@@ -17,5 +18,27 @@ public class Boss extends EnemyShip {
         frameCount++;
         if (frameCount % 10 == 0 || !isAlive)
             super.nextFrame();
+    }
+
+    @Override
+    public Bullet fire() {
+        if (isAlive) {
+            if (matrix == ShapeMatrix.BOSS_ANIMATION_FIRST) {
+                return new Bullet(x + 6, y + height, Direction.DOWN);
+            } else {
+                return new Bullet(x, y + height, Direction.DOWN);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void kill() {
+        if (isAlive) {
+            isAlive = false;
+            setAnimatedView(ShapeMatrix.KILL_BOSS_ANIMATION_FIRST,
+                    ShapeMatrix.KILL_BOSS_ANIMATION_SECOND,
+                    ShapeMatrix.KILL_BOSS_ANIMATION_THIRD);
+        }
     }
 }
